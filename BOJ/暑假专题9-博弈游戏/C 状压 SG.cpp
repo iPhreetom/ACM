b04a5f1ed6f51289ac7d5e5b36220327e1e3a89c
iPhreetom <iPhreetom@gmail.com>
2018-08-21 14:48:52 +0800

    add

---
+++ BOJ/暑假专题9-博弈游戏/C 状压 SG.cpp
@@ -0,0 +1,20 @@
+// fyt
+#include<bits/stdc++.h>
+#define int long long
+#define double long double
+using namespace std;
+
+void init(){
+	
+}
+
+void solve(){
+
+}
+
+signed main(){
+	ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+	int tt;cin>>tt;
+	while(tt--)solve();
+	return 0;
+}
