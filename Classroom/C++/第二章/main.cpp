eaf320f476480793afa7cc6e9c23ed682983c953
iPhreetom <iPhreetom@gmail.com>
2018-09-09 08:59:09 +0800

    18-9-9 AM

--- Classroom/C++/第二章/main.cpp
+++ Classroom/C++/第二章/main.cpp
@@ -25,6 +25,7 @@
 #include <complex>
 #include <numeric>
 #include <cassert>
+#include <windows.h>
 using namespace std;
 
 
@@ -38,7 +39,7 @@ public:
 		set<string> lis;
 		deque<string> message;
 		node(){};
-		node(string &phone , string &name){
+		node(string &name , string &phone){
 			this->phone = phone;
 			this->name = name;
 		};
@@ -47,9 +48,33 @@ public:
 	sys(){};
 
 	void begin(string file){
-		ifstream fin;
-		string address,name,phone,group;
-		while()
+		ifstream fin(file);
+		string address,name,phone,member;
+		int num;
+		while(fin>>address){
+			fin>>name>>phone>>num;
+			database[address] = (node){name,phone};
+			for (int i=0; i<num; i++){
+				fin>>member;
+				database[address].lis.insert(member);
+			}
+		}
+		fin.close();
+	}
+
+	void end(string file){
+		ofstream fout(file);
+		for(auto i:database){
+			fout<<i.first<<' ';
+			auto &t = i.second;
+			fout<<t.name<<' '<<t.phone<<' ';
+			fout<<t.lis.size()<<endl;
+			for(auto j:t.lis){
+				fout<<j<<' ';
+			}
+			fout<<endl;
+		}
+		fout.close();
 	}
 
 	void send(string &aim,string &message){
@@ -59,21 +84,47 @@ public:
 		}
 	}
 
-	void add_gruop(string &address , string &group){
+	bool list_information(string &address){
+		if(database.count(address)==0){
+			return false;
+		}
+		else{
+			if(!database[address].name.size() == 0){
+				cout<<"name = "<<database[address].name<<endl;
+			}
+			if(!database[address].phone.size() == 0){
+				cout<<"phone = "<<database[address].phone<<endl;
+			}
+			if(!database[address].lis.empty()){
+				cout<<"The member in it: "<<endl;
+				for(auto i:database[address].lis){
+					cout<<i<<endl;
+				}
+			}
+			return true;
+		}
+	}
+
+	bool add_gruop(string &address , string &group){
 		database[group].lis.insert(address);
 	}
 
-	void delete_gruop(string &address , string &group){
+	bool delete_gruop(string &address , string &group){
 		database[group].lis.erase(address);
 	}
 
-	void delete_person(string &address){
-		database.erase(address);
+	bool add_person(string &address , string &name , string &phone){
+		if(database.count(address) != 0)return false;
+		database[address] = (node){name,phone};
+		return true;
 	}
 
-	void add_person(string &address , string &name , string &phone){
-		database[address] = (node){name,phone};
+	bool delete_person(string &address){
+		if(database.count(address) != 0)return false;
+		database.erase(address);
+		return true;
 	}
+
 
 private:
 	map<string,node> database;
@@ -84,9 +135,83 @@ private:
 
 int main(){
 	sys bjtu;
-	sys.begin("data.txt");
+	// if(!fopen("data.txt","r")){
+	// 	auto f = fopen("data.txt","w");
+	// 	fclose(f);
+	// }
+	bjtu.begin("data.txt");
 
-
-	ofstream fout;
+	while(1){
+		system("cls");
+		cout<<"[1] - add person"<<endl;
+		cout<<"[2] - remove person"<<endl;
+		cout<<"[3] - join gruop"<<endl;
+		cout<<"[4] - leave gruop"<<endl;
+		cout<<"[5] - list information"<<endl;
+		cout<<"[0] - Exit"<<endl;
+		string input;
+		cin>>input;
+		if(input == "0"){
+			bjtu.end("data.txt");
+			break;
+		}
+		else if(input == "1"){
+			string email,phone,name;
+			cout<<"Input your Email: ";
+			cin>>email;
+			cout<<"Input your name: ";
+			cin>>name;
+			cout<<"Input your phonenumber: ";
+			cin>>phone;
+			if(!bjtu.add_person(email, name, phone)){
+				cout<<"The email is existed"<<endl;
+			}
+			else{
+				cout<<"Add successfully"<<endl;
+			}
+		}
+		else if(input == "2"){
+			string email;
+			cout<<"Input the email your want to remove from database: ";
+			cin>>email;
+			if(bjtu.delete_person(email)){
+				cout<<"The email does not exist"<<endl;
+			}
+			else{
+				cout<<"Remove successfully"<<endl;
+			}
+		}
+		else if(input == "3"){
+			string email,group;
+			cout<<"Input the gruop your want to join: ";
+			cin>>group;
+			cout<<"Input your email: ";
+			cin>>email;
+			bjtu.add_gruop(email, group);
+		}
+		else if(input == "4"){
+			string email,group;
+			cout<<"Input the gruop your want to leave: ";
+			cin>>group;
+			cout<<"Input your email: ";
+			cin>>email;
+			bjtu.delete_gruop(email, group);
+		}
+		else if(input == "5"){
+			string email;
+			cout<<"Input the email address and get the information: ";
+			cin>>email;
+			if(!bjtu.list_information(email)){
+				cout<<"The email does not exist"<<endl;
+			}
+			cout<<endl<<"Input anythings to continue"<<endl;
+			cin>>email;
+			continue;
+		}
+		else{
+			continue;
+		}
+		Sleep(1500);
+	}
 	return 0;
 }
--- Game/2018-ICPC-shenyang/I.cpp
+++ Game/2018-ICPC-shenyang/I.cpp
@@ -1,13 +1,168 @@
 #include<bits/stdc++.h>
-#define int long long
-#define double long double
-#define endl '\n'
 using namespace std;
 
 
-signed main(){
-	ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
-	int n;
-	
-	return 0;
+struct Trie
+{
+    int next[500010][2],fail[500010],end[500010];
+    int root,L;
+    int newnode()
+    {
+        for(int i = 0;i < 2;i++)
+            next[L][i] = -1;
+        end[L++] = 0;
+        return L-1;
+    }
+    void init()
+    {
+        L = 0;
+        root = newnode();
+    }
+    void insert(string &buf)
+    {
+        int len = buf.size();
+        int now = root;
+        for(int i = 0;i < len;i++)
+        {
+            if(next[now][buf[i]-'0'] == -1)
+                next[now][buf[i]-'0'] = newnode();
+            now = next[now][buf[i]-'0'];
+        }
+        end[now]++;
+    }
+    void build()
+    {
+        queue<int>Q;
+        fail[root] = root;
+        for(int i = 0;i < 2;i++)
+            if(next[root][i] == -1)
+                next[root][i] = root;
+            else
+            {
+                fail[next[root][i]] = root;
+                Q.push(next[root][i]);
+            }
+        while( !Q.empty() )
+        {
+            int now = Q.front();
+            Q.pop();
+            for(int i = 0;i < 2;i++)
+                if(next[now][i] == -1)
+                    next[now][i] = next[fail[now]][i];
+                else
+                {
+                    fail[next[now][i]]=next[fail[now]][i];
+                    Q.push(next[now][i]);
+                }
+        }
+    }
+    int query(string &buf)
+    {
+        int len = buf.size();
+        int now = root;
+        int res = 0;
+        for(int i = 0;i < len;i++)
+        {
+            now = next[now][buf[i]-'0'];
+            int temp = now;
+            while( temp != root )
+            {
+                res += end[temp];
+                // end[temp] = 0;
+                temp = fail[temp];
+            }
+        }
+        return res;
+    }
+    void debug()
+    {
+        for(int i = 0;i < L;i++)
+        {
+            printf("id = %3d,fail = %3d,end = %3d,chi = [",i,fail[i],end[i]);
+            for(int j = 0;j < 2;j++)
+                printf("%2d",next[i][j]);
+            printf("]\n");
+        }
+    }
+};
+Trie ac;
+
+
+int main(){
+	// printf("%c",33);
+	int tt;cin>>tt;
+	while(tt--){
+		int len,n;
+		map<string,char> mp;
+		string s,bits,finals,out;
+
+		cin>>len>>n;
+		for(int i=0;i<n;i++){
+			int ch;string code;
+			cin>>ch>>code;
+			mp[code] = (char)ch;
+		}
+
+		cin>>s;
+		for(char ch:s){
+			int num;
+			string b;
+			if(ch <= '9' && ch>= '0'){
+				num = ch-'0';
+			}
+			else{
+				if(ch <= 'Z' && ch >= 'A')
+					num = 10 + (ch-'A');
+				if(ch <= 'z' && ch >= 'a'){
+					num = 10 + (ch-'a');
+				}
+			}
+			while(num || b.size() < 4){
+				if(num&1 == 1)b.push_back('1');
+				else b.push_back('0');
+				num>>=1;
+			}
+			reverse(b.begin(), b.end());
+			bits += b;
+		}
+		// cout<<bits<<endl;
+
+		for(int i=8;i<bits.size();i+=9){
+			int num=0;
+			string b;
+			for(int j=i-8;j<i;j++){
+				if(bits[j] == '1')num++;
+				b.push_back(bits[j]);
+			}
+			if(num % 2 == 1 && bits[i] == '0'){
+				finals += b;
+			}
+			if(num % 2 == 0 && bits[i] == '1'){
+				finals += b;
+			}
+		}
+		// cout<<finals<<endl;
+		// cout<<finals<<endl;
+		// ac.init();
+		// for(auto i:mp){
+		// 	string t = i.first;
+		// 	cout<<"t = "<<t<<endl;
+		// 	ac.insert(t);
+		// }
+		// ac.build();
+
+		string c;
+		for(auto i:finals){
+			c.push_back(i);
+			if(mp.count(c) != 0){
+				// cout<<"c = "<<c<<endl;
+				// cout<<"mp[c] = "<<mp[c]<<endl;
+				out.push_back(mp[c]);
+				c.clear();
+			}
+			if(out.size() == len)break;
+		}
+		cout<<out<<endl;
+	}
+	return  0;
 }
