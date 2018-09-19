d50c1cdff174ac801222b63dd0cf675b6a284620
iPhreetom <34025769+iPhreetom@users.noreply.github.com>
2018-09-19 16:20:42 +0800

    18-9-17 PM

--- Classroom/C++/第二章/main.cpp
+++ Classroom/C++/第二章/main.cpp
@@ -37,26 +37,38 @@ public:
 		string phone;
 		string name;
 		set<string> lis;
+		string email_type;
 		deque<string> message;
 		node(){};
+		node(string &name){
+			this->name = name;
+			this->email_type = "group";
+		};
 		node(string &name , string &phone){
 			this->phone = phone;
 			this->name = name;
+			this->email_type = "general";
 		};
+
 	};
 
 	sys(){};
 
 	void begin(string file){
 		ifstream fin(file);
-		string address,name,phone,member;
+		string address,name,phone,member,email_type;
 		int num;
 		while(fin>>address){
-			fin>>name>>phone>>num;
-			database[address] = (node){name,phone};
-			for (int i=0; i<num; i++){
-				fin>>member;
-				database[address].lis.insert(member);
+			fin>>name>>phone>>email_type;
+			if(email_type == "general"){
+				database[address] = (node){name,phone};
+			}
+			else{
+				database[address] = (node){name};
+				for (int i=0; i<num; i++){
+					fin>>member;
+					database[address].lis.insert(member);
+				}
 			}
 		}
 		fin.close();
@@ -67,7 +79,13 @@ public:
 		for(auto i:database){
 			fout<<i.first<<' ';
 			auto &t = i.second;
-			fout<<t.name<<' '<<t.phone<<' ';
+			fout<<t.name<<' ';
+
+			if(t.phone.empty())cout<<"----";
+			else cout<<t.phone;
+
+			fout<<' '<<t.email_type<<' ';
+
 			fout<<t.lis.size()<<endl;
 			for(auto j:t.lis){
 				fout<<j<<' ';
@@ -82,10 +100,30 @@ public:
 	}
 
 
-	void send(string &aim,string &message){
-		database[aim].message.push_back(message);
-		for(auto i: database[aim].lis){
-			send(aim,message);
+	// void send(string &aim,string &message){
+	// 	database[aim].message.push_back(message);
+	// 	for(auto i: database[aim].lis){
+	// 		send(aim,message);
+	// 	}
+	// }
+
+	void find_member(string address,set<string> &has_member, set<string> &has_group){
+		queue<string> que;
+		que.push(address);
+		while(!que.empty()){
+			address = que.front();
+			que.pop();
+			for(string i:database[address].lis){
+				if(database[i].email_type == "general"){
+					has_member.insert(i);
+				}
+				else{
+					if(has_group.count(i) == 0){
+						has_group.insert(i);
+						que.push(i);
+					}
+				}
+			}
 		}
 	}
 
@@ -101,20 +139,26 @@ public:
 				cout<<"phone = "<<database[address].phone<<endl;
 			}
 			if(!database[address].lis.empty()){
-				cout<<"The member in it: "<<endl;
-				for(auto i:database[address].lis){
+				set<string> has_group , has_member;
+				cout<<"The general member under it: "<<endl;
+				find_member(address,has_member,has_group);
+				for(auto i:has_member){
 					cout<<i<<endl;
+				}
+				cout<<"The group member under it: "<<endl;
+				for(auto i:has_group){
+					if(i!=address)cout<<i<<endl;
 				}
 			}
 			return true;
 		}
 	}
 
-	bool add_gruop(string &address , string &group){
+	bool join_group(string &address , string &group){
 		database[group].lis.insert(address);
 	}
 
-	bool delete_gruop(string &address , string &group){
+	bool leave_group(string &address , string &group){
 		database[group].lis.erase(address);
 	}
 
@@ -126,6 +170,29 @@ public:
 
 	bool delete_person(string &address){
 		if(database.count(address) != 0)return false;
+		for(auto i:database){
+			if(i.second.lis.count(address)!=0){
+				i.second.lis.erase(address);
+			}
+		}
+		database.erase(address);
+		return true;
+	}
+
+	bool add_group(string &address , string &name ,set<string> member_list){
+		if(database.count(address) != 0)return false;
+		database[address] = (node){name};
+		database[address].lis = member_list;
+		return true;
+	}
+
+	bool delete_group(string &address){
+		if(database.count(address) != 0)return false;
+		for(auto i:database){
+			if(i.second.lis.count(address)!=0){
+				i.second.lis.erase(address);
+			}
+		}
 		database.erase(address);
 		return true;
 	}
@@ -148,11 +215,13 @@ int main(){
 
 	while(1){
 		system("cls");
-		cout<<"[1] - add person"<<endl;
-		cout<<"[2] - remove person"<<endl;
-		cout<<"[3] - join gruop"<<endl;
-		cout<<"[4] - leave gruop"<<endl;
-		cout<<"[5] - list information"<<endl;
+		cout<<"[1] - create person account"<<endl;
+		cout<<"[2] - remove person account"<<endl;
+		cout<<"[3] - create group account"<<endl;
+		cout<<"[4] - remove group account"<<endl;
+		cout<<"[5] - join a group"<<endl;
+		cout<<"[6] - leave a group"<<endl;
+		cout<<"[7] - list one's information"<<endl;
 		cout<<"[0] - Exit"<<endl;
 		string input;
 		cin>>input;
@@ -187,33 +256,69 @@ int main(){
 			}
 		}
 		else if(input == "3"){
-			string email,group;
-			cout<<"Input the gruop your want to join: ";
-			cin>>group;
-			cout<<"Input your email: ";
+			string email,name;
+			set<string> lis;
+			int number;
+			cout<<"Input your Email: ";
 			cin>>email;
-			bjtu.add_gruop(email, group);
+			cout<<"Input your name: ";
+			cin>>name;
+			cout<<"Input the initial number of email address in it :";
+			cin>>number;
+			cout<<"Input the member's email address :";
+			for (int i=0; i<number; i++){
+				string one;
+				cin>>one;
+				lis.insert(one);
+			}
+			if(!bjtu.add_group(email, name, lis)){
+				cout<<"The email is existed"<<endl;
+			}
+			else{
+				cout<<"Add successfully"<<endl;
+			}
 		}
 		else if(input == "4"){
+			string email;
+			cout<<"Input the group your want to remove from database: ";
+			cin>>email;
+			if(bjtu.delete_group(email)){
+				cout<<"The email does not exist"<<endl;
+			}
+			else{
+				cout<<"Remove successfully"<<endl;
+			}
+		}
+		else if(input == "5"){
 			string email,group;
-			cout<<"Input the gruop your want to leave: ";
+			cout<<"Input the group your want to join: ";
 			cin>>group;
 			cout<<"Input your email: ";
 			cin>>email;
-			bjtu.delete_gruop(email, group);
+			bjtu.join_group(email, group);
 		}
-		else if(input == "5"){
+		else if(input == "6"){
+			string email,group;
+			cout<<"Input the group your want to leave: ";
+			cin>>group;
+			cout<<"Input your email: ";
+			cin>>email;
+			bjtu.leave_group(email, group);
+		}
+		else if(input == "7"){
 			string email;
 			cout<<"Input the email address and get the information: ";
 			cin>>email;
 			if(!bjtu.list_information(email)){
 				cout<<"The email does not exist"<<endl;
 			}
-			cout<<endl<<"Input anythings to continue"<<endl;
-			cin>>email;
+			cout<<endl<<"Press anythings tocontinue"<<endl;
+			// cin>>email;
+			getchar();
+			getchar();
 			continue;
 		}
-		else{
+		else {
 			continue;
 		}
 		Sleep(1500);
