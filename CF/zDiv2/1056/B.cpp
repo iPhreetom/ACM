d8dd0f3f6edf25e088d1e74058c25e7c27050918
iPhreetom <iPhreetom@gmail.com>
2018-11-28 13:22:35 +0800

    18-11-28 noon

--- CF/zDiv2/1056/B.cpp
+++ CF/zDiv2/1056/B.cpp
@@ -8,6 +8,22 @@ using namespace std;
 
 signed main(){
 	ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
-	
+	int n,m;
+	cin>>n>>m;
+	int ans = 0;
+	for (int i=1; i<=m; i++){
+		for (int j=1; j<=m; j++){
+
+			int res = n%m;
+			int re = n/m;
+			int num1 = re + (res>=i?1:0);
+			int num2 = re + (res>=j?1:0);
+			if((i*i+j*j)%m == 0){
+				ans += num1*num2;
+			}
+
+		}
+	}
+	cout<<ans<<endl;
 	return 0;
 }
