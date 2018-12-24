1bec21515004760a567cdb8bdbcf3400c577aacb
iPhreetom <34025769+iPhreetom@users.noreply.github.com>
2018-12-24 10:41:45 +0800

    somethings

---
+++ Classroom/C++/第五次作业 重载运算符/STRING.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define int long long
+#define double long double
+using namespace std;
+
+class string {
+private:
+	char s[1e6+5];
+public:
+	s();
+	s(char *st){
+		strcmp(s,st);
+	}
+	s(const char )
+}
+signed main(){
+	ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+
+	return 0;
+}
--- z-Other/your.cpp
+++ z-Other/your.cpp
@@ -1,15 +1,70 @@
-// fyt
 #include<bits/stdc++.h>
-#define int long long
-#define double long double
-#define endl '\n'
-// #define int long long
 using namespace std;
 
-int a[maxn];
+class hero {
+protected:
+	int number;
+	int position;
+	int hp;
+	int ad;
+public:
+	hero(){};
+	hero(int number, int position, int hp, int ad){
+		this->number = number;
+		this->position = position;
+		this->hp = hp;
+		this->ad = ad;
+	}
+	~hero(){};
+	virtual void forward() = 0;
+	virtual int attack() = 0;
+	virtual int counterattact() = 0;
+	virtual void bigboom() = 0;
+};
 
-signed main(){
-	ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+class dragon: public hero {
+private:
 
+public:
+	dragon(){};
+	dragon(int number, int position, int hp, int ad){
+		this->number = number;
+		this->position = position;
+		this->hp = hp;
+		this->ad = ad;
+	}
+	~dragon(){};
+	void forward(){
+		cout<<"Dragon go"<<endl;
+	}
+	int attack(){
+		return 1;
+	}
+	int counterattact(){
+		return 1;
+	}
+	void bigboom(){
+
+	}
+};
+
+
+void War(){
+	// queue<hero*> que;
+	// dragon a;
+	// hero *p = &a;
+	// que.push(p);
+	// que.front()->forward();
+	
+}
+
+
+int main(){
+	int case_number;
+	cin>>case_number;
+	for (int i=1; i<=case_number; i++){
+		cout<<"Case "<<case_number<<":\n";
+		War();
+	}
 	return 0;
 }
